073680fb5205924a2844d5ca65cda3d8f390c35c
John DeNero <papajohn@gmail.com>
2015-02-26 15:46:32 -0800

    limit to lowercase

--- src/main/java/org/oscii/Main.java
+++ src/main/java/org/oscii/Main.java
@@ -5,6 +5,7 @@ import org.oscii.panlex.PanLexJSONParser;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class Main {
@@ -16,12 +17,14 @@ public class Main {
         Options options = new Options();
         options.addOption("r", "read", true, "read JSON file");
         options.addOption("w", "write", true, "write JSON file");
-        options.addOption("p", "panlex", true, "parse PanLex JSON");
         options.addOption("s", "serve", false, "listen on local rabbitmq");
         options.addOption("h", "host", true, "rabbitmq host");
         options.addOption("q", "queue", true, "rabbitmq queue");
         options.addOption("u", "username", true, "rabbitmq username");
         options.addOption("a", "password", true, "rabbitmq password");
+        options.addOption("p", "panlex", true, "parse PanLex JSON");
+        options.addOption("pattern", true, "expression pattern");
+        options.addOption("l", "languages", true, "expression pattern");
         CommandLineParser parser = new BasicParser();
         CommandLine line = parser.parse( options, args );
 
@@ -30,9 +33,19 @@ public class Main {
         if (line.hasOption("p")) {
             String path = line.getOptionValue("p");
             PanLexJSONParser panLex = new PanLexJSONParser(path);
-            // TODO(denero) Add "languages" command-line option
-            panLex.addLanguages(Arrays.asList("en", "es", "fr", "de"));
-            panLex.read(null);
+
+            List<String> languages = Arrays.asList("en", "es", "fr", "de");
+            if (line.hasOption("l")) {
+                languages = Arrays.asList(line.getOptionValue("l").split(" "));
+            }
+            panLex.addLanguages(languages);
+
+            Pattern pattern = Pattern.compile("(?U)\\p{Lower}*");
+            if (line.hasOption("pattern")) {
+                pattern = Pattern.compile(line.getOptionValue("pattern"));
+            }
+            panLex.read(pattern);
+
             panLex.yieldTranslations(lexicon::add);
         } else if (line.hasOption("r")) {
             lexicon.read(new File(line.getOptionValue("r")));
