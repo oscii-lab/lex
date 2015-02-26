b5b43145514e33fe291f2b7080e4abcfb72482cf
John DeNero <papajohn@gmail.com>
2015-02-26 15:04:52 -0800

    command-line parsing

--- src/main/java/org/oscii/Main.java
+++ src/main/java/org/oscii/Main.java
@@ -1,5 +1,6 @@
 package org.oscii;
 
+import org.apache.commons.cli.*;
 import org.oscii.panlex.PanLexJSONParser;
 
 import java.io.File;
@@ -10,32 +11,35 @@ public class Main {
 
     public static void main(String[] args)
             throws java.io.IOException,
-            java.lang.InterruptedException {
-        // TODO Real command-line argument parsing
-        String path = args[args.length - 1];
-        boolean readPanLex = Arrays.asList(args).contains("-p");
-        boolean writePanLex = Arrays.asList(args).contains("-w");
-        boolean rabbit = Arrays.asList(args).contains("-r");
+            java.lang.InterruptedException,
+            ParseException {
+        Options options = new Options();
+        options.addOption("r", "read", true, "read JSON file");
+        options.addOption("w", "write", true, "write JSON file");
+        options.addOption("p", "panlex", true, "parse PanLex JSON");
+        options.addOption("s", "serve", false, "listen on local rabbitmq");
+        CommandLineParser parser = new BasicParser();
+        CommandLine line = parser.parse( options, args );
 
         Lexicon lexicon = new Lexicon();
 
-        if (readPanLex) {
+        if (line.hasOption("p")) {
+            String path = line.getOptionValue("p");
             PanLexJSONParser panLex = new PanLexJSONParser(path);
+            // TODO(denero) Add "languages" command-line option
             panLex.addLanguages(Arrays.asList("en", "es", "fr", "de"));
-            panLex.read(Pattern.compile("a.*"));
             panLex.yieldTranslations(lexicon::add);
-            if (writePanLex) {
-                lexicon.write(new File("panlex.json"));
-            }
-        } else {
-            lexicon.read(new File(path));
+        } else if (line.hasOption("r")) {
+            lexicon.read(new File(line.getOptionValue("r")));
         }
 
-        if (rabbit) {
+        if (line.hasOption("w")) {
+            lexicon.write(new File(line.getOptionValue("w")));
+        }
+
+        if (line.hasOption("s")) {
             RabbitHandler handler = new RabbitHandler("localhost", "lexicon", lexicon);
             handler.ConnectAndListen();
         }
-
-        System.out.println("Done");
     }
 }
