ae5814932712eb6b0bac5988aafaf3e5f9a8c240
John DeNero <papajohn@gmail.com>
2015-03-03 22:59:02 -0800

    Indexing and frequency counts

--- src/main/java/org/oscii/concordance/AlignedCorpus.java
+++ src/main/java/org/oscii/concordance/AlignedCorpus.java
@@ -1,6 +1,8 @@
 package org.oscii.concordance;
 
 import com.codepoetics.protonpack.StreamUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,14 +11,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
  * Index and compute statistics over an aligned corpus.
  */
 public class AlignedCorpus {
-    // Directory containing sentence and alignment files
     private String path;
     private String sourceSuffix;
     private String targetSuffix;
@@ -24,6 +28,10 @@ public class AlignedCorpus {
     List<AlignedSentence> sentences = new ArrayList<>();
     Map<String, List<Location>> sourceIndex = new HashMap<>();
     Map<String, List<Location>> targetIndex = new HashMap<>();
+    Map<String, Map<String, Long>> targetsBySource = new HashMap<>();
+    Map<String, Map<String, Long>> sourcesByTarget = new HashMap<>();
+
+    private final static Logger log = LogManager.getLogger(AlignedCorpus.class);
 
     public AlignedCorpus(String path, String sourceSuffix, String targetSuffix) {
         this.path = path;
@@ -31,21 +39,83 @@ public class AlignedCorpus {
         this.targetSuffix = targetSuffix;
     }
 
+    /*
+     * Read and index a parallel corpus.
+     */
     public void read() throws IOException {
+        log.info("Reading sentence pairs");
         Stream<String> sources = Files.lines(Paths.get(path + "." + sourceSuffix));
         Stream<String> targets = Files.lines(Paths.get(path + "." + targetSuffix));
         Stream<String> aligns = Files.lines(Paths.get(path + ".align"));
         sentences = StreamUtils.zip(sources, targets, aligns,
                 (s, t, a) -> AlignedSentence.create(s, t, a))
                 .collect(Collectors.toList());
+        tally();
+    }
+
+    /*
+     * Create indices and counts.
+     */
+    public void tally() {
+        log.info("Indexing sentence pairs");
+        sourceIndex = indexTokens(AlignedCorpus::sourceTokens);
+        targetIndex = indexTokens(AlignedCorpus::targetTokens);
+        log.info("Counting alignments");
+        targetsBySource = countLinks(sourceIndex, AlignedCorpus::alignedTarget);
+        sourcesByTarget = countLinks(targetIndex, AlignedCorpus::alignedSource);
+    }
+
+    /*
+     * Count one-to-one alignments.
+     */
+    private static Map<String, Map<String, Long>> countLinks(
+            Map<String, List<Location>> index,
+            BiFunction<AlignedSentence, Integer, String> aligned) {
+        return index.entrySet().stream().collect(Collectors.toMap(
+                e -> e.getKey(),
+                e -> e.getValue().stream().map((Location loc) ->
+                        aligned.apply(loc.sentence, loc.tokenIndex))
+                        .filter(s -> s != null)
+                        .collect(Collectors.groupingBy(Function.identity(),
+                                Collectors.counting()))));
+    }
+
+    /*
+     * Index tokens of sentences by their type.
+     */
+    private Map<String, List<Location>> indexTokens(
+            Function<AlignedSentence, List<String>> tokens) {
+        return sentences.stream().flatMap(s ->
+                IntStream.range(0, tokens.apply(s).size())
+                        .mapToObj((int j) -> new Location(s, j)))
+                .collect(Collectors.groupingBy(loc ->
+                        tokens.apply(loc.sentence).get(loc.tokenIndex)));
+    }
+
+    /* Helper functions TODO Can these be replaced? */
+
+    private static List<String> sourceTokens(AlignedSentence s) {
+        return s.sourceTokens;
+    }
+
+    private static List<String> targetTokens(AlignedSentence s) {
+        return s.targetTokens;
+    }
+
+    private static String alignedTarget(AlignedSentence s, Integer pos) {
+        return s.alignedTarget(pos);
+    }
+
+    private static String alignedSource(AlignedSentence s, Integer pos) {
+        return s.alignedSource(pos);
     }
 
     private class Location {
-        int sentenceIndex;
+        AlignedSentence sentence;
         int tokenIndex;
 
-        public Location(int sentenceIndex, int tokenIndex) {
-            this.sentenceIndex = sentenceIndex;
+        public Location(AlignedSentence sentence, int tokenIndex) {
+            this.sentence = sentence;
             this.tokenIndex = tokenIndex;
         }
     }
--- src/main/java/org/oscii/concordance/AlignedSentence.java
+++ src/main/java/org/oscii/concordance/AlignedSentence.java
@@ -2,6 +2,7 @@ package org.oscii.concordance;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -11,19 +12,58 @@ public class AlignedSentence {
     public final List<String> sourceTokens;
     public final List<String> targetTokens;
     public final List<Link> links;
+    public final Map<Integer, List<Link>> sourceByTarget;
+    public final Map<Integer, List<Link>> targetBySource;
 
     public AlignedSentence(List<String> sourceTokens, List<String> targetTokens, List<Link> links) {
         this.sourceTokens = sourceTokens;
         this.targetTokens = targetTokens;
         this.links = links;
+        sourceByTarget = links.stream().collect(Collectors.groupingBy(
+                link -> new Integer(link.sourcePos)));
+        targetBySource = links.stream().collect(Collectors.groupingBy(
+                link -> new Integer(link.targetPos)));
     }
 
+    /*
+     * Create an aligned sentence from space-delimited strings.
+     */
     public static AlignedSentence create(String source, String target, String align) {
         return new AlignedSentence(
                 Arrays.asList(source.split("\\s")),
                 Arrays.asList(target.split("\\s")),
                 Arrays.asList(align.split("\\s")).stream()
                         .map(Link::parse).collect(Collectors.toList()));
+    }
+
+    /*
+     * One-to-one aligned target word for source position
+     */
+    public String alignedTarget(int sourceIndex) {
+        List<Link> sourceLinks = targetBySource.get(sourceIndex);
+        if (sourceLinks != null && sourceLinks.size() == 1) {
+            int target = sourceLinks.get(0).targetPos;
+            List<Link> targetLinks = sourceByTarget.get(target);
+            if (targetLinks != null && targetLinks.size() == 1) {
+                return targetTokens.get(target);
+            }
+        }
+        return null;
+    }
+
+    /*
+     * One-to-one aligned source word for target position
+     */
+    public String alignedSource(int targetIndex) {
+        List<Link> targetLinks = sourceByTarget.get(targetIndex);
+        if (targetLinks != null && targetLinks.size() == 1) {
+            int source = targetLinks.get(0).sourcePos;
+            List<Link> sourceLinks = targetBySource.get(source);
+            if (sourceLinks != null && sourceLinks.size() == 1) {
+                return sourceTokens.get(source);
+            }
+        }
+        return null;
     }
 
     private static class Link {
