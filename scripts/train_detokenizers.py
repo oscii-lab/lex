6e0da1692a9dcea5df5169961fe85f6701a88044
John DeNero <papajohn@gmail.com>
2015-06-21 09:26:00 -0700

    set training, memory, and regularization

--- scripts/train_detokenizers.py
+++ scripts/train_detokenizers.py
@@ -9,13 +9,14 @@ raw = 'data/detok/detok_sample_1M_{}_raw.clean.gz'
 tok = 'data/detok/detok_sample_1M_{}_raw.clean.tok.gz'
 out = 'data/detok/detok_sample_1M_{}_raw.clean.detokenizer'
 java = 'java -cp build/install/lex/lib/*'.split()
-mem =  '-Xmx3g -XX:+UseG1GC -XX:+UseStringDeduplication'.split()
+mem =  '-Xmx4g -XX:+UseG1GC -XX:+UseStringDeduplication'.split()
 cls = ['org.oscii.detokenize.TrainDetokenizer']
 
 if __name__ == '__main__':
     subprocess.check_call(['gradle', 'installDist'])
     for lang in langs:
         opt = ['-raw', raw.format(lang), '-tok', tok.format(lang), '-out', out.format(lang)]
+        opt += ['-trainsize', '400000', '-regularization', '400']
         cmd = java + mem + cls + opt
         subprocess.check_call(cmd)
         
