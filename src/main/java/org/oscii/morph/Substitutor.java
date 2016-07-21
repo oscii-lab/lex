91940a5ed9eeb93061e3d7f8a73a03602c7d545a
John DeNero <papajohn@gmail.com>
2016-07-21 10:44:06 -0700

    score in parallel and turn off debug logging.

--- src/main/java/org/oscii/morph/Substitutor.java
+++ src/main/java/org/oscii/morph/Substitutor.java
@@ -81,7 +81,7 @@ public class Substitutor {
 
     public void scoreRules(RuleScored.ScoringParams params) {
         log.info("Scoring substitutions");
-        scored = substitutions.entrySet().stream()
+        scored = substitutions.entrySet().parallelStream()
                 .map(e -> scoreRule(e.getKey(), e.getValue(), params))
                 .sorted((r, s) -> Double.compare(s.hitRate, r.hitRate))
                 .collect(toList());
